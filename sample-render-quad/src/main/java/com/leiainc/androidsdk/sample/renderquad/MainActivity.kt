7243f2725ffe7823a5d24736236e9dfbf8c7111c
Clinton Pinto <ctpinto27@gmail.com>
2020-11-17 14:42:05 -0800

    Add simple Quad sample

--- sample-render-quad/src/main/java/com/leiainc/androidsdk/sample/renderquad/MainActivity.kt
+++ sample-render-quad/src/main/java/com/leiainc/androidsdk/sample/renderquad/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.leiainc.androidsdk.sample.renderquad
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.leiainc.androidsdk.core.QuadView
+import com.leiainc.androidsdk.core.ScaleType
+import com.leiainc.androidsdk.display.LeiaDisplayManager
+import com.leiainc.androidsdk.display.LeiaSDK
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quadView: QuadView = findViewById(R.id.quad_view)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.optician_2x2)
+
+        quadView.scaleType = ScaleType.FIT_CENTER
+        quadView.setQuadBitmap(bitmap)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val displayManager: LeiaDisplayManager? = LeiaSDK.getDisplayManager(applicationContext)
+        displayManager?.requestBacklightMode(LeiaDisplayManager.BacklightMode.MODE_2D)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val displayManager: LeiaDisplayManager? = LeiaSDK.getDisplayManager(applicationContext)
+        displayManager?.requestBacklightMode(LeiaDisplayManager.BacklightMode.MODE_3D)
+
+        /*  Make app full screen */
+        setFullScreenImmersive()
+    }
+
+    private fun setFullScreenImmersive() {
+        val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        val decorView = window.decorView
+        decorView.clearFocus()
+        decorView.systemUiVisibility = flags
+
+        // Code below is to handle presses of Volume up or Volume down.
+        // Without this, after pressing volume buttons, the navigation bar will
+        // show up and won't hide
+        decorView.setOnSystemUiVisibilityChangeListener { visibility: Int ->
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                decorView.systemUiVisibility = flags
+            }
+        }
     }
 }
