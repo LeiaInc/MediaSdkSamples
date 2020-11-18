0ad35b22d4b288abc8b7508e32d98619449c88d3
Clinton Pinto <ctpinto27@gmail.com>
2020-11-17 20:55:11 -0800

    Initial add project

---
+++ sample-render-lif/src/main/java/com/leiainc/androidsdk/sample/sample_render_lif/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leiainc.androidsdk.sample.sample_render_lif
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
