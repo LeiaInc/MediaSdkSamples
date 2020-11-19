39902f48479dd60cd740fcb6f890aef2870c6b7f
Clinton Pinto <ctpinto27@gmail.com>
2020-11-18 18:10:05 -0800

    Add SBS demo

---
+++ sample-render-sbs/src/main/java/com/leiainc/androidsdk/sample/sample_render_sbs/SbsRenderDemoActivity.kt
@@ -0,0 +1,11 @@
+package com.leiainc.androidsdk.sample.sample_render_sbs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SbsRenderDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sbs_render_demo)
+    }
+}
