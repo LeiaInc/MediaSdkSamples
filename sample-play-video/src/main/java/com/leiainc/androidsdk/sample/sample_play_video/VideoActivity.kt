04b16ee13df68ed3779be1a42cb686999a04da3e
Clinton Pinto <ctpinto27@gmail.com>
2020-11-19 13:25:32 -0800

    Initial commit play video

---
+++ sample-play-video/src/main/java/com/leiainc/androidsdk/sample/sample_play_video/VideoActivity.kt
@@ -0,0 +1,11 @@
+package com.leiainc.androidsdk.sample.sample_play_video
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
